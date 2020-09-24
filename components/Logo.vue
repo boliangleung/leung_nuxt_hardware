560e74aa13b6046527acbd31294928561d3f1629
liangboliang <liangboliang@yy.com>
2020-09-24 15:26:09 +0800

    feat:使用nuxt框架初始化

---
+++ components/Logo.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+  {{'你号'}}
+  </div>
+</template>
+
+<style>
+.NuxtLogo {
+  animation: 1s appear;
+  margin: auto;
+}
+
+@keyframes appear {
+  0% {
+    opacity: 0;
+  }
+}
+</style>
